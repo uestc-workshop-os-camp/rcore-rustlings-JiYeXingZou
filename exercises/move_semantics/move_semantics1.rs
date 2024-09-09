77365863efb9b8e02002d4917f3850fb45c2ec82
sagiri <2581279733@qq.com>
2024-09-09 21:00:27 +0800

    update

--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -3,12 +3,15 @@
 // Execute `rustlings hint move_semantics1` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+// I AM NOT DONE 
+//栈内存  vs  堆内存
 
+//栈 -> 编译期识别类型的大小，默认分配的空间
+//堆 -> 运行时动态分配
 fn main() {
-    let vec0 = Vec::new();
+    let vec0 = Vec::new(); // 动态增长
 
-    let vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(vec0);  
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
