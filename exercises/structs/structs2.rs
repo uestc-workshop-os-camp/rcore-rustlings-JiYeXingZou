0aeb1c323bbfeced4525a41dc7cc890be7805e1e
sagiri <2581279733@qq.com>
2024-09-12 19:05:02 +0800

    update

--- exercises/structs/structs2.rs
+++ exercises/structs/structs2.rs
@@ -5,7 +5,7 @@
 // Execute `rustlings hint structs2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+//
 
 #[derive(Debug)]
 struct Order {
@@ -38,7 +38,15 @@ mod tests {
     fn your_order() {
         let order_template = create_order_template();
         // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+        let your_order = Order{
+            name: String::from("Hacker in Rust"),
+            year: 2019,
+            made_by_phone: false,
+            made_by_mobile: false,
+            made_by_email: true,
+            item_number: 123,
+            count: 1,
+        };
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
         assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
@@ -48,3 +56,16 @@ mod tests {
         assert_eq!(your_order.count, 1);
     }
 }
+/*
+String 与字符串字面量的区别：
+
+字符串字面量（例如 "Hacker in Rust"）是一个不可变的、固定大小的字符串，它的类型是 &str。这种字符串存储在程序的只读内存中，无法动态改变长度。
+String 是 Rust 中的可变字符串类型，存储在堆上，它可以在运行时动态地调整大小。String 是一个拥有字符串数据的类型，具有更多的功能和灵活性，例如可以扩展、修改等。
+为什么要使用 String::from()：
+
+结构体 Order 中的 name 字段类型是 String，而字面量 "Hacker in Rust" 是 &str。为了将 &str 转换为 String 类型，使用了 String::from()。
+String::from() 方法的作用是将 &str 转换为堆上分配的 String，从而使得 name 字段可以保存并拥有这个字符串。
+直接使用字符串字面量的问题：
+
+如果直接将 "Hacker in Rust" 赋给 name，会出现类型不匹配错误，因为 "Hacker in Rust" 的类型是 &str，而 name 需要的是 String。
+*/
